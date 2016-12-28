92be6f78ef0e40f0f6e60b9662f38acd4d014c3d
Eli Bendersky <eliben@gmail.com>
2016-12-28 06:58:00 -0800

    Cleanup

--- luz_asm_sim/lib/asmlib/objectfile.py
+++ luz_asm_sim/lib/asmlib/objectfile.py
@@ -1,6 +1,6 @@
 # Represents the object file recognized by the Luz architecture.
-# An object file is relocatable. It is created by the assembler, 
-# and later combined with other object files by the linker into 
+# An object file is relocatable. It is created by the assembler,
+# and later combined with other object files by the linker into
 # an executable.
 #
 # Luz micro-controller assembler
@@ -8,10 +8,10 @@
 #
 
 class ObjectFile(object):
-    """ Use one of the factory methods to create ObjectFile 
+    """ Use one of the factory methods to create ObjectFile
         instances: from_assembler, from_file
-        
-        The name of the object can be accessed via the .name 
+
+        The name of the object can be accessed via the .name
         attribute.
     """
     def __init__(self):
@@ -19,14 +19,14 @@ class ObjectFile(object):
         self.export_table = []
         self.import_table = []
         self.reloc_table = []
-        
+
         self.name = None
-    
+
     @classmethod
-    def from_assembler( cls, 
-                        seg_data, 
-                        export_table, 
-                        import_table, 
+    def from_assembler( cls,
+                        seg_data,
+                        export_table,
+                        import_table,
                         reloc_table):
         """ Create a new ObjectFile from assembler-generated data
             structures.
@@ -35,17 +35,16 @@ class ObjectFile(object):
         assert isinstance(seg_data, dict)
         for table in (export_table, import_table, reloc_table):
             assert isinstance(table, list)
-        
+
         obj.seg_data = seg_data
         obj.export_table = export_table
         obj.import_table = import_table
         obj.reloc_table = reloc_table
         return obj
-    
+
     @classmethod
     def from_file(cls, file):
         """ 'file' is either a filename (a String), or a readable
             IO object.
         """
         pass
-
