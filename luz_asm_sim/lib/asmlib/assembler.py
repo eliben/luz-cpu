c9ae00be0dd2eb82b05260e50afd7a5a3c5a9f93
Eli Bendersky <eliben@gmail.com>
2016-12-28 06:32:27 -0800

    Cosmetic cleanup

--- luz_asm_sim/lib/asmlib/assembler.py
+++ luz_asm_sim/lib/asmlib/assembler.py
@@ -11,9 +11,7 @@
 import pprint, os, sys
 from collections import defaultdict
 
-from ..commonlib.utils import (
-    word2bytes, num_fits_in_nbits,
-    unpack_bytes)
+from ..commonlib.utils import word2bytes, num_fits_in_nbits, unpack_bytes
 
 from .asmparser import (
     AsmParser, Instruction, Directive, Id, Number, String)
@@ -40,7 +38,7 @@ class Assembler(object):
         """ Assembles the code given in a string or a file.
             Returns an ObjectFile instance with the results of the
             assembly process.
-        
+
             Provide the code as a string in str. If str is None
             the code is assumed to reside in a file named by
             filename.
@@ -54,48 +52,48 @@ class Assembler(object):
         # 1. Parse assembly code into an intermediate format
         #
         imf = self._parse(str)
-        
+
         # 2. First pass
         #
         symtab, addr_imf = self._compute_addresses(imf)
-        
+
         # 3. Second pass
         #
         return self._assemble_code(symtab, addr_imf)
 
     ######################--   PRIVATE   --######################
-    
+
     def _parse(self, str):
         return self.parser.parse(str)
-    
+
     def _compute_addresses(self, imf):
         """ First pass of the assembler.
-            Takes an intermediate form representation of the 
+            Takes an intermediate form representation of the
             parsed assembly code (imf).
-            
+
             Builds a symbol table mapping labels defined in the
             code into addresses. The symbol table maps labels
             to SegAddr objects.
-            
+
             Returns a pair: symbol table and an addressed IMF,
             a list of (addr, imf_line) pairs, with addr being the
             address into which the line is to be assembled.
             Note: .segment directives are removed from this list,
             since after the first pass they're no longer required.
-            
+
             Does the minimal amount of work to infer the address
             of each label:
-            
+
             * Recognizes directives and takes into account those
-              that allocate storage, and those that specify 
+              that allocate storage, and those that specify
               segments
-            * Recognizes instructions and takes into account their 
+            * Recognizes instructions and takes into account their
               lengths
-            
+
             Therefore, the error checking performed here is only
             of the constructs required for construction the symbol
             table:
-            
+
             * Recognition of unknown instructions
             * Validation of only those directives that affect
               address computations.
@@ -106,23 +104,23 @@ class Assembler(object):
         # Symbol table
         # Maps label names to SegAddr objects
         #
-        symtab = {}     
-        
+        symtab = {}
+
         # Addressed imf
         # Stores pairs (seg_addr, imf_line) for each line in the
         # intermediate form. seg_addr is a SegAddr object
         #
-        addr_imf = []   
-        
+        addr_imf = []
+
         # Stores the current address for each segment. All start
         # at 0 when the segment is first defined.
         #
         seg_addr = {}
-        
+
         # The current segment
         #
         cur_seg = None
-        
+
         for line in imf:
             # If there's no "current segment", this line can only
             # be a .segment definition (unlabeled, because this
@@ -136,21 +134,21 @@ class Assembler(object):
             else:
                 # Current segment and its running address
                 saddr = SegAddr(cur_seg, seg_addr[cur_seg])
-            
+
             # If there's a label, make sure it's not duplicated
             # and add it to the symbol table
             #
             if line.label:
                 if line.label in symtab:
                     self._assembly_error("label '%s' duplicated" % line.label, line.lineno)
-                
+
                 symtab[line.label] = saddr
 
             if isinstance(line, Instruction):
                 if line.name is None:
                     # Labels without instructions don't affect
                     # the address
-                    # They don't have to be added to addr_imf 
+                    # They don't have to be added to addr_imf
                     # either
                     #
                     pass
@@ -162,13 +160,13 @@ class Assembler(object):
             elif isinstance(line, Directive):
                 if cur_seg and line.name != '.segment':
                     addr_imf.append((saddr, line))
-                
+
                 if line.name == '.segment':
-                    # Switch to the segment named in the 
-                    # directive. If it's a new segment, its 
+                    # Switch to the segment named in the
+                    # directive. If it's a new segment, its
                     # address count starts at 0.
                     #
-                    self._validate_args(line, [Id])                    
+                    self._validate_args(line, [Id])
                     cur_seg = line.args[0].id
                     if not cur_seg in seg_addr:
                         seg_addr[cur_seg] = 0
@@ -185,19 +183,19 @@ class Assembler(object):
                     seg_addr[cur_seg] = self._align_at_next_word(addr)
                 elif line.name == '.string':
                     self._validate_args(line, [String])
-                    # +1 for the zero termination that will be 
-                    # inserted when the string is allocated in 
+                    # +1 for the zero termination that will be
+                    # inserted when the string is allocated in
                     # the second pass
                     #
                     addr = seg_addr[cur_seg] + len(line.args[0].val) + 1
                     seg_addr[cur_seg] = self._align_at_next_word(addr)
             else:
                 self._assembly_error("bad assembly", line.lineno)
-        
+
         return symtab, addr_imf
-    
+
     def _assemble_code(self, symtab, addr_imf):
-        """ Second pass of the assembler. 
+        """ Second pass of the assembler.
             Utilizes the symbol table and the addressed IMF lines
             pre-computed in the first pass to assemble the code
             into object data.
@@ -205,15 +203,15 @@ class Assembler(object):
         # Holds constants defined with the .define directive
         #
         defines = {}
-        
+
         # The assembled data as a list of bytes, per segment
         #
         seg_data = defaultdict(list)
-        
+
         export_table = []
         import_table = []
         reloc_table = []
-        
+
         # addr: a SegAddr object
         # line: the parsed IMF for a line of assembly
         #
@@ -223,45 +221,45 @@ class Assembler(object):
                 # in the right place?
                 #
                 assert len(seg_data[addr.segment]) == addr.offset
-                
+
                 # Assemble the instruction. This returns a list
                 # of AssembledInstruction objects.
                 #
                 try:
                     asm_instrs = assemble_instruction(
-                        line.name, line.args, addr, 
+                        line.name, line.args, addr,
                         symtab, defines)
                 except InstructionError:
                     err = sys.exc_info()[1]
                     self._assembly_error(err, line.lineno)
-                
+
                 for asm_instr in asm_instrs:
                     # The offset in the segment this instruction
                     # will be placed into
                     #
                     offset = len(seg_data[addr.segment])
-                    
+
                     if asm_instr.import_req:
                         type, symbol = asm_instr.import_req
                         # note that we're using offset, and not
                         # addr.offset here, because this could be
                         # a pseudo-instruction assembling to two
-                        # instructions, and addr.offset points 
+                        # instructions, and addr.offset points
                         # only to the first one.
                         #
                         import_table.append(ImportEntry(
                             import_symbol=symbol,
                             type=type,
-                            addr=SegAddr(addr.segment, offset)))                    
-                    
+                            addr=SegAddr(addr.segment, offset)))
+
                     if asm_instr.reloc_req:
                         type, segment = asm_instr.reloc_req
                         reloc_table.append(RelocEntry(
                             reloc_segment=segment,
                             type=type,
                             addr=SegAddr(addr.segment, offset)))
-                    
-                    # Add the assembled instruction into the 
+
+                    # Add the assembled instruction into the
                     # segment data
                     #
                     seg_data[addr.segment].extend(word2bytes(asm_instr.op))
@@ -270,12 +268,12 @@ class Assembler(object):
                     # Add the defined symbol to the defines table,
                     # possibly overriding a previous definition
                     #
-                    self._validate_args(line, [Id, Number])                    
+                    self._validate_args(line, [Id, Number])
                     defines[line.args[0].id] = line.args[1].val
                 elif line.name == '.global':
                     self._validate_args(line, [Id])
                     symbol_name = line.args[0].id
-                    
+
                     # The exported symbol must be present in the
                     # symbol table collected by the first pass
                     #
@@ -286,14 +284,14 @@ class Assembler(object):
                     else:
                         self._assembly_error('.global defines an unknown label %s' % symbol_name, line.lineno)
                 elif line.name == '.alloc':
-                    # The arguments of .alloc directives were 
+                    # The arguments of .alloc directives were
                     # validated in the first pass
                     #
                     num = self._align_at_next_word(line.args[0].val)
                     seg_data[addr.segment].extend([0] * num)
                 elif line.name == '.byte':
                     data = []
-                    
+
                     for i, byte_arg in enumerate(line.args):
                         if (isinstance(byte_arg, Number) and
                             num_fits_in_nbits(byte_arg.val, 8)
@@ -301,15 +299,15 @@ class Assembler(object):
                             data.append(byte_arg.val)
                         else:
                             self._assembly_error('.byte -- argument %s not a valid byte' % (i + 1,), line.lineno)
-                 
+
                     leftover = len(data) % 4
                     if leftover:
                         data.extend([0] * (4 - leftover))
-                    
+
                     seg_data[addr.segment].extend(data)
                 elif line.name == '.word':
                     data = []
-                    
+
                     for i, word_arg in enumerate(line.args):
                         if (isinstance(word_arg, Number) and
                             num_fits_in_nbits(word_arg.val, 32)
@@ -317,14 +315,14 @@ class Assembler(object):
                             data.extend(word2bytes(word_arg.val))
                         else:
                             self._assembly_error('.word -- argument %s not a valid word' % (i + 1,), line.lineno)
-                    
+
                     seg_data[addr.segment].extend(data)
                 elif line.name == '.string':
                     data = unpack_bytes(line.args[0].val + '\x00')
                     leftover = len(data) % 4
                     if leftover:
                         data.extend([0] * (4 - leftover))
-                    
+
                     seg_data[addr.segment].extend(data)
                 else:
                     # .segment directives should not be passed
@@ -332,20 +330,20 @@ class Assembler(object):
                     #
                     if line.name == '.segment':
                         assert 0
-                        
+
                     self._assembly_error('unknown directive %s' % line.name, line.lineno)
             else:
                 self._assembly_error("bad assembly", line.lineno)
 
         return ObjectFile.from_assembler(
-                    seg_data=seg_data, 
-                    export_table=export_table, 
-                    import_table=import_table, 
+                    seg_data=seg_data,
+                    export_table=export_table,
+                    import_table=import_table,
                     reloc_table=reloc_table)
-    
+
     def _validate_args(self, line, exp_args):
-        """ Validates that the arguments of the 
-            directive stored in 'line' are of the correct amount 
+        """ Validates that the arguments of the
+            directive stored in 'line' are of the correct amount
             and types.
         """
         if len(exp_args) != len(line.args):
@@ -354,15 +352,15 @@ class Assembler(object):
         for i, exp_type in enumerate(exp_args):
             if not isinstance(line.args[i], exp_type):
                 self._assembly_error("%s -- argument '%s' of unexpected type" % (line.name, line.args[i]), line.lineno)
-    
+
     def _no_segment_error(self, lineno):
         self._assembly_error("A segment must be defined before this line", lineno)
-    
+
     def _assembly_error(self, msg, lineno):
        raise AssemblyError("%s (at line %s)" % (msg, lineno))
-    
+
     def _align_at_next_word(self, addr):
-        """ Make sure an address is aligned at a word (4-byte) 
+        """ Make sure an address is aligned at a word (4-byte)
             boundary. If it isn't, align it at the next word.
         """
         offset = addr % 4;
@@ -370,4 +368,3 @@ class Assembler(object):
             return addr
         else:
             return addr + (4 - offset)
-
